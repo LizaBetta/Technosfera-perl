7f1521a784bc5b2b75803a9838bd2aca8da236b1
LizaBetta <e.liza.betta@mail.ru>
2016-10-08 17:13:29 +0300

    Add project

--- homeworks/music_library/lib/Local/Table.pm
+++ homeworks/music_library/lib/Local/Table.pm
@@ -41,26 +41,66 @@ sub max_width
         }
     }
 1;}
-
-
-sub push_str
+    
+sub filter
 {
-    my ($data, $table, $col_width, $filter_type, $filter_name) = @_;
-    my %str = Local::CutStr::cut_str($data);
+    my ($str, $filter_name, $filter_type) = @_;
     if (defined($filter_name))
     {
         if ($filter_type eq 'year')
         {
-            if (0 + $str{$filter_type} != $filter_name)
+            if (0 + $str->{$filter_type} != $filter_name)
             {
-                return $table;
+                return 1;
             }
         }
-        elsif ($str{$filter_type} ne $filter_name)
+        elsif ($str->{$filter_type} ne $filter_name)
         {
-            return $table;
+            return 1;
+        }
+    } 
+    return 0;
+1;}
+
+sub print_line
+{
+    my ($width, $order) = @_;
+    print "|";
+    foreach my $k (0..$#$order)
+    {
+        foreach (1..$width->{$order->[$k]})
+        {
+            print '-';
+        }
+        if ($k != $#$order)
+        {
+            print "+";
         }
     }
+    print "|\n";
+}
+
+sub print_words
+{
+    my ($str, $width, $order) = @_;
+    print '|';
+    foreach my $j (@$order)
+    {
+        my $string = $str->{ $j };
+        my $el_width = $width->{ $j };         
+        foreach (1 .. $el_width - length($string) - 1)
+        {
+            print ' ';
+        }
+        print  "$string |";
+    }
+    print "\n";
+}
+sub push_str
+{
+    my ($data, $table, $col_width, $filter_type, $filter_name) = @_;
+    my %str = Local::CutStr::cut_str($data);
+    if(filter(\%str, $filter_name, $filter_type)) { return 0; }
     max_width(\%str, $col_width);
     if (scalar(@$table) != 0)
     { 
@@ -70,72 +110,34 @@ sub push_str
     {
          %{ $table -> [ 0 ] } = %str;
     }
-
     return $table;
 1;} 
 
+
 sub print_table
 {
-    my $table = shift;
-    my $width = shift;
-    my $order = shift;
-    my $table_width = scalar(@$order) + 1;
+    my ($table, $width, $order) = @_;
+    my $table_width = scalar(@$order) + 1;#учитываем перегородки
     my $t_rows = scalar(@$table);
-    
-    if($t_rows == 0 or scalar(@$order) == 0)
-    {
-        return;
-    }
-    
+    if($t_rows == 0 or scalar(@$order) == 0){ return; }
     foreach my $v (@$order)
     {
         $table_width += $width->{$v};
     }
-    
     print '/';
-    
-    foreach(2..$table_width - 1)
-    {
-        print "-";
-    }
+    foreach(2..$table_width - 1){ print "-"; }
     print "\\\n";
+    
     foreach my $i (0..$t_rows - 1)
     {
-        print '|';
-        foreach my $j (@$order)
-        {
-            my $string = ${ $table->[$i] }{ $j };
-            my $el_width = $width->{ $j };
-            
-            foreach (1 .. $el_width - length($string) - 1)
-            {
-                print ' ';
-            }
-            print  "$string |";
-        }
-        print "\n";
+        print_words($table->[$i], $width, $order);
         if($i != $t_rows - 1)
         {
-            print "|";
-            foreach my $k (0..$#$order)
-            {
-                foreach (1..$width->{$order->[$k]})
-                {
-                    print '-';
-                }
-                if ($k != $#$order)
-                {
-                    print "+";
-                }
-            }
-            print "|\n";
+            print_line($width, $order);
         }
     }
     print '\\';
-    foreach (2..$table_width - 1)
-    {
-        print "-";
-    }
+    foreach (2..$table_width - 1){ print "-"; }
     print "/\n";
 1;}
 
